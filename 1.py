bbe74dd213be1e1856f818590491f96ee14355f3
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2023-03-16 00:37:14 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlretrieve
-from zipfile import ZipFile
-import os
-url = f'https://data.binance.vision/?prefix=data/spot/monthly/klines/ETHUSDT/1h/ETHUSDT-1h-{year}-{mont_one}.zip'
-urlretrieve(url, '/home/admin1/PycharmProjects/pythonProject1/123.zip')
-with ZipFile('123.zip', 'r') as zip_file:
-    zip_file.extractall('/home/admin1/PycharmProjects/pythonProject1/file')
-os.remove('/home/admin1/PycharmProjects/pythonProject1/123.zip')
